577fa4a3191cf82d05b9bfd66525782d2fd5cccd
Balint Szikora <chamaselion@gmail.com>
2025-04-20 13:50:51 +0200

    Created derived class from scavtrap and implemented the new functions for it as well. Added basic tests for main for now

---
+++ ex01/includes/ScavTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 17:31:42 by bszikora          #+#    #+#             */
+/*   Updated: 2025/04/20 13:26:31 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class ScavTrap : public ClapTrap
+{
+private:
+	bool gatekeep;
+	
+public:
+	ScavTrap();
+	ScavTrap(const std::string &name);
+	ScavTrap(const ScavTrap& other);
+	ScavTrap& operator=(const ScavTrap& other);
+	~ScavTrap();
+
+	void attack(const std::string& target);
+	void guardGate();
+};
+
+#endif
---
+++ ex01/sources/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 16:33:18 by bszikora          #+#    #+#             */
+/*   Updated: 2025/04/20 13:47:12 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int main()
+{
+    ScavTrap scav1("ScavOne");
+    ScavTrap scav2("ScavTwo");
+
+    scav1.attack("ScavTwo");
+    scav2.takeDamage(20);
+	
+	scav1.guardGate();
+	scav2.guardGate();
+	
+    scav2.attack("ScavOne");
+    scav1.takeDamage(15);
+
+    scav1.beRepaired(10);
+    scav2.beRepaired(5);
+
+    scav1.attack("ScavTwo");
+    scav2.takeDamage(30);
+
+    scav2.attack("ScavOne");
+    scav1.takeDamage(50);
+
+    scav1.beRepaired(20);
+    scav2.beRepaired(10);
+
+    return 0;
+}
